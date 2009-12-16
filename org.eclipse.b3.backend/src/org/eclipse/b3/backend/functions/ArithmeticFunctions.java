e1f8dcbdafc44eb3af181dc1a1e066528f3a40f4
Michal Ruzicka <michal.ruzicka@cloudsmith.com>
2009-12-16 16:35:24 +0000

    - support BigInteger and Byte in bitwise operations - guard against unsupported argument types

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/functions/ArithmeticFunctions.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/functions/ArithmeticFunctions.java
@@ -2,6 +2,7 @@ package org.eclipse.b3.backend.functions;
 
 import java.lang.reflect.Type;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 
 import org.eclipse.b3.backend.core.B3Backend;
 import org.eclipse.b3.backend.evaluator.b3backend.BExecutionContext;
@@ -140,104 +141,121 @@ public class ArithmeticFunctions {
 					+","+ b.getClass().toString());
 	}
 
-	@B3Backend (funcNames={"&"}, guardFunction="denyFloatingPointGuard")
+	@B3Backend (funcNames={"&"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseAnd(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() & b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() & b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() & b.shortValue()));
-		}
+		if(a instanceof BigInteger && alwaysTrue(b = convertToBigIntegerIfNeeded(b)) ||
+				b instanceof BigInteger && alwaysTrue(a = convertToBigInteger(a)))
+			return ((BigInteger)a).and((BigInteger)b);
+		if(a instanceof Long || b instanceof Long)
+			return new Long(a.longValue() & b.longValue());
+		if(a instanceof Integer || b instanceof Integer)
+			return new Integer(a.intValue() & b.intValue());
+		if(a instanceof Short || b instanceof Short)
+			return new Short((short) (a.shortValue() & b.shortValue()));
+		if(a instanceof Byte || b instanceof Byte)
+			return new Byte((byte) (a.byteValue() & b.byteValue()));
 		throw new IllegalArgumentException("Bitwise AND of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
 
-	@B3Backend (funcNames={"|"})
+	@B3Backend (funcNames={"|"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseOr(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() | b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() | b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() | b.shortValue()));
-		}
+		if(a instanceof BigInteger && alwaysTrue(b = convertToBigIntegerIfNeeded(b)) ||
+				b instanceof BigInteger && alwaysTrue(a = convertToBigInteger(a)))
+			return ((BigInteger)a).or((BigInteger)b);
+		if(a instanceof Long || b instanceof Long)
+			return new Long(a.longValue() | b.longValue());
+		if(a instanceof Integer || b instanceof Integer)
+			return new Integer(a.intValue() | b.intValue());
+		if(a instanceof Short || b instanceof Short)
+			return new Short((short) (a.shortValue() | b.shortValue()));
+		if(a instanceof Byte || b instanceof Byte)
+			return new Byte((byte) (a.byteValue() | b.byteValue()));
 		throw new IllegalArgumentException("Bitwise OR of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
 
-	@B3Backend (funcNames={"^"})
+	@B3Backend (funcNames={"^"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseXor(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() ^ b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() ^ b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() ^ b.shortValue()));
-		}
+		if(a instanceof BigInteger && alwaysTrue(b = convertToBigIntegerIfNeeded(b)) ||
+				b instanceof BigInteger && alwaysTrue(a = convertToBigInteger(a)))
+			return ((BigInteger)a).xor((BigInteger)b);
+		if(a instanceof Long || b instanceof Long)
+			return new Long(a.longValue() ^ b.longValue());
+		if(a instanceof Integer || b instanceof Integer)
+			return new Integer(a.intValue() ^ b.intValue());
+		if(a instanceof Short || b instanceof Short)
+			return new Short((short) (a.shortValue() ^ b.shortValue()));
+		if(a instanceof Byte || b instanceof Byte)
+			return new Byte((byte) (a.byteValue() ^ b.byteValue()));
 		throw new IllegalArgumentException("Bitwise XOR of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
 
-	@B3Backend (funcNames={"<<"})
+	@B3Backend (funcNames={"<<"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseLeftShift(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() << b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() << b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() << b.shortValue()));
-		}
+		if(a instanceof BigInteger)
+			return ((BigInteger)a).shiftLeft(b.intValue());
+		if(a instanceof Long)
+			return new Long(a.longValue() << b.intValue());
+		if(a instanceof Integer)
+			return new Integer(a.intValue() << b.intValue());
+		if(a instanceof Short)
+			return new Short((short) (a.shortValue() << b.intValue()));
+		if(a instanceof Byte)
+			return new Byte((byte) (a.byteValue() << b.intValue()));
 		throw new IllegalArgumentException("Bitwise LSHIFT of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
-	@B3Backend (funcNames={">>"})
+	@B3Backend (funcNames={">>"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseRightShift(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() >> b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() >> b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() << b.shortValue()));
-		}
+		if(a instanceof BigInteger)
+			return ((BigInteger)a).shiftRight(b.intValue());
+		if(a instanceof Long)
+			return new Long(a.longValue() >> b.intValue());
+		if(a instanceof Integer)
+			return new Integer(a.intValue() >> b.intValue());
+		if(a instanceof Short)
+			return new Short((short) (a.shortValue() >> b.intValue()));
+		if(a instanceof Byte)
+			return new Byte((byte) (a.byteValue() >> b.intValue()));
 		throw new IllegalArgumentException("Bitwise RSHIFT of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
 
-	@B3Backend (funcNames={">>>"})
+	@B3Backend (funcNames={">>>"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseUnsignedRightShift(Number a, Number b) {
-		if(!(a instanceof Double || b instanceof Double || a instanceof Float || b instanceof Float)) {
-			if(a instanceof Long || b instanceof Long)
-				return new Long(a.longValue() >>> b.longValue());
-			if(a instanceof Integer || b instanceof Integer)
-				return new Integer(a.intValue() >>> b.intValue());
-			if(a instanceof Short || b instanceof Short)
-				return new Short((short) (a.shortValue() >>> b.shortValue()));
-		}
+		if(a instanceof BigInteger)
+			return ((BigInteger)a).shiftRight(b.intValue());
+		if(a instanceof Long)
+			return new Long(a.longValue() >>> b.intValue());
+		if(a instanceof Integer)
+			return new Integer(a.intValue() >>> b.intValue());
+		if(a instanceof Short)
+			return new Short((short) (a.shortValue() >>> b.intValue()));
+		if(a instanceof Byte)
+			return new Byte((byte) (a.byteValue() >>> b.intValue()));
 		throw new IllegalArgumentException("Bitwise Unsigned RSHIFT of unsupported Number subclass: "
 					+a.getClass().toString()
 					+","+ b.getClass().toString());
 	}
-	@B3Backend (funcNames={"~"})
+	@B3Backend (funcNames={"~"}, guardFunction="permitIntegralOnlyGuard")
 	public static Number bitwiseComplement(Number a) {
-		if(!(a instanceof Double || a instanceof Float)) {
-			if(a instanceof Long)
-				return new Long(~a.longValue());
-			if(a instanceof Integer)
-				return new Integer(~a.intValue());
-			if(a instanceof Short)
-				return new Short((short) ~(a.shortValue()));
-		}
+		if(a instanceof BigInteger)
+			return ((BigInteger)a).not();
+		if(a instanceof Long)
+			return new Long(~a.longValue());
+		if(a instanceof Integer)
+			return new Integer(~a.intValue());
+		if(a instanceof Short)
+			return new Short((short) ~(a.shortValue()));
+		if(a instanceof Byte)
+			return new Byte((byte) ~(a.byteValue()));
 		throw new IllegalArgumentException("Unary bitwize complement (~) on unsupported Number subclass: "
 					+a.getClass().toString());
 	}
@@ -258,15 +276,38 @@ public class ArithmeticFunctions {
 					+a.getClass().toString());
 	}
 	
-	@B3Backend(guard=true)
-	public static Boolean denyFloatingPointGuard(BExecutionContext ctx, Object[] parameters, Type[] types) {
+	@B3Backend (guard=true)
+	public static Boolean permitIntegralOnlyGuard(BExecutionContext ctx, Object[] parameters, Type[] types) {
 		if(types == null)
-			return Boolean.TRUE; // don't mind no parameters 
+			return Boolean.TRUE; // don't mind no parameters
 		for(int i = 0; i < types.length; i++) {
-			Class<?> clazz = TypeUtils.getRaw(types[i]);
-			if(clazz == Float.class || clazz == Double.class || clazz == BigDecimal.class)
-				return Boolean.FALSE;
+			if(parameters[i] != null) {
+				// TODO: TYPESYSTEM IMPROVEMENT - checking the types of the actual arguments is a hack we want to check
+				// the types passed in the types parameter directly but we need them to be accurate first
+				Class<?> clazz = parameters[i].getClass();
+				if(!(clazz == Byte.class || clazz == Short.class || clazz == Integer.class || clazz == Long.class || clazz == BigInteger.class))
+					return Boolean.FALSE;
+			} else {
+				Class<?> clazz = TypeUtils.getRaw(types[i]);
+				if(clazz == Float.class || clazz == Double.class || clazz == BigDecimal.class)
+					return Boolean.FALSE;
+			}
 		}
 		return Boolean.TRUE;
+	}
+
+	private static boolean alwaysTrue(Object ignored)
+	{
+		return true;
+	}
+
+	private static BigInteger convertToBigIntegerIfNeeded(Number n)
+	{
+		return (n instanceof BigInteger) ? (BigInteger)n : convertToBigInteger(n);
+	}
+
+	private static BigInteger convertToBigInteger(Number n)
+	{
+		return BigInteger.valueOf(n.longValue());
 	}
 }
