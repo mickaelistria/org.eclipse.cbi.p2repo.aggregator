bdbe33b720075f2cd2bc0bb59fffe11fe25cedae
Michal Ruzicka <michal.ruzicka@cloudsmith.com>
2009-12-16 16:57:00 +0000

    improve exception message formatting

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3NotAcceptedByGuardException.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3NotAcceptedByGuardException.java
@@ -6,19 +6,20 @@ public class B3NotAcceptedByGuardException extends B3EngineException {
 
 	private static final long serialVersionUID = 1L;
 	public B3NotAcceptedByGuardException() {
-		this(" unknown", new Type[] {});
+		this("unknown", new Type[] {});
 	}
 	public B3NotAcceptedByGuardException(String functionName, Type[] types) {
-		super("Available function(s) with suitable signature rejected by guard. Function: "+functionName +"', for types :" + typestring(types));
+		super("Available function(s) with suitable signature rejected by guard. Function: '" + functionName + "', for types: " + typestring(types));
 	}
 	public static String typestring(Type[] types) {
-		StringBuffer buffer = new StringBuffer();
+		StringBuilder buffer = new StringBuilder();
 		buffer.append('[');
 		for(int i = 0; i < types.length; i++) {
 			if(i > 0)
 				buffer.append(", ");
 			buffer.append(types[i].toString());
 		}
+		buffer.append(']');
 		return buffer.toString();
 	}
 }
