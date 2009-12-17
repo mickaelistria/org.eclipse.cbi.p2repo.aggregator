f1dd88dbeb598287c9d5bfffdc1e135c1936bbd7
Michal Ruzicka <michal.ruzicka@cloudsmith.com>
2009-12-17 19:56:11 +0000

    create a (and use) console for B3 language execution output

---
+++ org.eclipse.b3.beelang.ui/src/org/eclipse/b3/beelang/ui/BeeLangConsoleUtils.java
@@ -0,0 +1,49 @@
+package org.eclipse.b3.beelang.ui;
+
+import java.io.PrintStream;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.console.MessageConsoleStream;
+
+public class BeeLangConsoleUtils {
+
+	public static final String BEE_LANG_CONSOLE = "B3 Console"; 
+
+	public static MessageConsole getBeeLangConsole() {
+		IConsoleManager consoleManager = ConsolePlugin.getDefault().getConsoleManager();
+		IConsole[] existing = consoleManager.getConsoles();
+
+		for(int i = 0; i < existing.length; i++)
+			if(BEE_LANG_CONSOLE.equals(existing[i].getName()))
+				return (MessageConsole) existing[i];
+
+		// no console found, so create a new one
+		MessageConsole beeLangConsole = new MessageConsole(BEE_LANG_CONSOLE, null);
+		// TODO: preference page defining the console colors
+		// beeLangConsole.setBackground(???);
+		consoleManager.addConsoles(new IConsole[] { beeLangConsole });
+		return beeLangConsole;
+	}
+
+	public static PrintStream getConsoleOutputStream(MessageConsole console) {
+		MessageConsoleStream stream = console.newMessageStream();
+		// TODO: preference page defining the console colors
+		// stream.setColor(???);
+		return new PrintStream(stream);
+	}
+
+	public static PrintStream getConsoleErrorStream(MessageConsole console) {
+		MessageConsoleStream stream = console.newMessageStream();
+		// TODO: preference page defining the console colors
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		stream.setColor(display.getSystemColor(SWT.COLOR_RED));
+		return new PrintStream(stream);
+	}
+
+}
--- org.eclipse.b3.beelang.ui/src/org/eclipse/b3/commands/ExecuteHandler.java
+++ org.eclipse.b3.beelang.ui/src/org/eclipse/b3/commands/ExecuteHandler.java
@@ -1,5 +1,6 @@
 package org.eclipse.b3.commands;
 
+import java.io.PrintStream;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +10,13 @@ import org.eclipse.b3.backend.evaluator.b3backend.B3JavaImport;
 import org.eclipse.b3.backend.evaluator.b3backend.BFunction;
 import org.eclipse.b3.backend.evaluator.typesystem.TypeUtils;
 import org.eclipse.b3.beeLang.BeeModel;
+import org.eclipse.b3.beelang.ui.BeeLangConsoleUtils;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.expressions.EvaluationContext;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.ui.console.MessageConsole;
 import org.eclipse.xtext.ui.common.editor.outline.ContentOutlineNode;
 import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
@@ -21,48 +24,57 @@ public class ExecuteHandler extends AbstractHandler {
 
 	@SuppressWarnings("unchecked")
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		System.out.print("Running the main function...\n");
-		EvaluationContext ctx = (EvaluationContext)event.getApplicationContext();
-		List<ContentOutlineNode> nodes = (List<ContentOutlineNode>)ctx.getDefaultVariable();
-		ContentOutlineNode node = nodes.get(0);
-		Object result = node.getEObjectHandle().readOnly(new IUnitOfWork<Object, EObject>() {
+		final MessageConsole b3Console = BeeLangConsoleUtils.getBeeLangConsole();
+		PrintStream b3ConsoleOutputStream = BeeLangConsoleUtils.getConsoleOutputStream(b3Console);
+		try {
+			b3ConsoleOutputStream.println("Running the main function...");
+			EvaluationContext ctx = (EvaluationContext) event.getApplicationContext();
+			List<ContentOutlineNode> nodes = (List<ContentOutlineNode>) ctx.getDefaultVariable();
+			ContentOutlineNode node = nodes.get(0);
+			Object result = node.getEObjectHandle().readOnly(new IUnitOfWork<Object, EObject>(){
+				public Object exec(EObject state) throws Exception {
+					B3Engine engine = new B3Engine();
+					// Define all functions, and
+					// find a function called main (use the first found) and call it with a List<Object> argv
+					BFunction main = null;
+					for(BFunction f : ((BeeModel) state).getFunctions()) {
+						engine.getContext().defineFunction(f);
+						if("main".equals(f.getName())) {
+							main = f;
+						}
+					}
+					// Define all imports as constants
+					for(Type t : ((BeeModel) state).getImports()) {
+						if(t instanceof B3JavaImport) {
+							B3JavaImport ji = (B3JavaImport) t;
+							Class<?> x = TypeUtils.getRaw(ji);
+							engine.getContext().defineValue(((B3JavaImport) t).getName(), x, x);
+						}
+					}
+					if(main == null)
+						return null;
+					final List<Object> argv = new ArrayList<Object>();
+					try {
+						return engine.getContext().callFunction("main", new Object[] { argv },
+								new Type[] { List.class });
+					} catch(Throwable e) {
+						// Just print some errors
+						e.printStackTrace();
+						PrintStream b3ConsoleErrorStream = BeeLangConsoleUtils.getConsoleErrorStream(b3Console);
+						try {
+							b3ConsoleErrorStream.println(e.getMessage());
+						} finally {
+							b3ConsoleErrorStream.close();
+						}
+					}
 
-			public Object exec(EObject state) throws Exception {
-				B3Engine engine = new B3Engine();
-				// Define all functions, and 
-				// find a function called main (use the first found) and call it with a List<Object> argv
-				BFunction main = null;
-				for(BFunction f : ((BeeModel)state).getFunctions()) {
-					engine.getContext().defineFunction(f);
-					if("main".equals(f.getName())) {
-						main = f;
-					}
-				}
-				// Define all imports as constants
-				for( Type t : ((BeeModel)state).getImports()) {
-					if(t instanceof B3JavaImport) {
-						B3JavaImport ji = (B3JavaImport)t;
-						Class<?> x = TypeUtils.getRaw(ji);
-						engine.getContext().defineValue(((B3JavaImport)t).getName(), 
-								x, x);
-					}
-				}
-				if(main == null)
 					return null;
-				final List<Object> argv = new ArrayList<Object>();
-				try {
-					return engine.getContext().callFunction("main", new Object[] { argv }, new Type[] { List.class });
-				} catch (Throwable e) {
-					// Just print some errors
-					e.printStackTrace();
 				}
-				
-				return null;
-			}
+			});
+			b3ConsoleOutputStream.println("Result = " + (result == null ? "null" : result.toString()));
+			return null;
+		} finally {
+			b3ConsoleOutputStream.close();
 		}
-		);
-		System.out.print("Result = " + (result == null ? "null" : result.toString())+"\n");
-		return null;
 	}
-
 }
