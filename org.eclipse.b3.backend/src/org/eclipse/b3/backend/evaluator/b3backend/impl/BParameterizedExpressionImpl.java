6147f7ef9ac258eee4c10c2ab51f10ac7a05bfe8
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2009-12-20 03:04:18 +0000

    Removed warnings.

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/BParameterizedExpressionImpl.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/BParameterizedExpressionImpl.java
@@ -6,24 +6,19 @@
  */
 package org.eclipse.b3.backend.evaluator.b3backend.impl;
 
-import java.util.Collection;
 
 import org.eclipse.b3.backend.evaluator.b3backend.B3backendFactory;
 import org.eclipse.b3.backend.evaluator.b3backend.B3backendPackage;
 import org.eclipse.b3.backend.evaluator.b3backend.BParameterList;
-import org.eclipse.b3.backend.evaluator.b3backend.BExpression;
 import org.eclipse.b3.backend.evaluator.b3backend.BParameterizedExpression;
 
 import org.eclipse.emf.common.notify.Notification;
 import org.eclipse.emf.common.notify.NotificationChain;
 
-import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.InternalEObject;
 
 import org.eclipse.emf.ecore.impl.ENotificationImpl;
-import org.eclipse.emf.ecore.util.EObjectContainmentEList;
-import org.eclipse.emf.ecore.util.InternalEList;
 
 /**
  * <!-- begin-user-doc -->
@@ -151,7 +146,6 @@ public abstract class BParameterizedExpressionImpl extends BExpressionImpl imple
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
-	@SuppressWarnings("unchecked")
 	@Override
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
