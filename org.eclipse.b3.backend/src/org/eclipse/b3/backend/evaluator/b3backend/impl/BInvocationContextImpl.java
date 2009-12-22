ae173a3e32cb30a207b4b291aae628d1c5788b5a
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2009-12-22 21:46:40 +0000

    Removed warnings

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/BInvocationContextImpl.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/BInvocationContextImpl.java
@@ -6,12 +6,10 @@
  */
 package org.eclipse.b3.backend.evaluator.b3backend.impl;
 
-import java.lang.reflect.Type;
 import java.util.Properties;
 
 import org.eclipse.b3.backend.core.B3EngineException;
 import org.eclipse.b3.backend.core.B3ExpressionCache;
-import org.eclipse.b3.backend.core.LValue;
 import org.eclipse.b3.backend.evaluator.b3backend.B3backendPackage;
 import org.eclipse.b3.backend.evaluator.b3backend.BCachedExpression;
 import org.eclipse.b3.backend.evaluator.b3backend.BExecutionContext;
