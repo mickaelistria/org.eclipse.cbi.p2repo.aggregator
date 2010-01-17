5dc1d73b59cd5a6feb03ae90aee4972b50e7b333
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-01-17 02:40:05 +0000

    Dead code removal.

--- org.eclipse.b3.beelang/src/org/eclipse/b3/validation/BeeLangJavaValidator.java
+++ org.eclipse.b3.beelang/src/org/eclipse/b3/validation/BeeLangJavaValidator.java
@@ -13,14 +13,10 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EPackage;
 
 import org.eclipse.xtext.validation.Check;
-//import org.eclipse.equinox.internal.provisional.p2.core.Version;
-//import org.eclipse.equinox.internal.provisional.p2.core.VersionRange;
  
-
-//@SuppressWarnings("restriction")
 public class BeeLangJavaValidator extends AbstractBeeLangJavaValidator {
 	
-@Override
+	@Override
 	protected List<EPackage> getEPackages() {
 		List<EPackage> result = super.getEPackages();
         result.add(B3backendPackage.eINSTANCE);
@@ -28,20 +24,7 @@ public class BeeLangJavaValidator extends AbstractBeeLangJavaValidator {
         return result;
 
 	}
-//	@Check
-//	public void checkFunctionDeclaredWithFunctionKeyword(Function func) {
-//		if (func.getFunc().getForm().equals("method")) {
-//			error("Methods can only be defined in a unit, or a context", func.getFunc(), BeeLangPackage.FUNCTION_OR_METHOD__FORM);
-//		}
-//	}
-//	@Check
-//	public void checkMethodDeclaredWithMethodKeyword(Method func) {
-//		if (func.getMethod().getForm().equals("function")) {
-//			error("Functions can only be defined in global scope, or in a concern",
-//					func.getMethod(),
-//					BeeLangPackage.FUNCTION_OR_METHOD__FORM);
-//		}
-//	}
+
 	@Check
 	public void checkWithClauseIsNotEmpty(BWithExpression withClause) {
 		if (withClause.getReferencedAdvice().size() == 0 
@@ -64,37 +47,7 @@ public class BeeLangJavaValidator extends AbstractBeeLangJavaValidator {
 					B3BuildPackage.BUILDER__NAME);
 		}
 	}
-//	@Check
-//	public void checkVersion(org.eclipse.b3.beeLang.Version version) {
-//		String versionString = version.getOriginal();
-//		try {
-//		Version.create(versionString);
-//		}
-//		catch(IllegalArgumentException e)
-//			{
-//			error("Version '"+versionString+"' is not a valid version: "+e.getMessage(),
-//					version,
-//					BeeLangPackage.VERSION__ORIGINAL);
-//		}
-//	}
-//	@Check
-//	public void checkVersionRange(org.eclipse.b3.beeLang.VersionRange versionRange) {
-//		String versionString = versionRange.getOriginal();
-//		
-////
-////		String versionString = versionRange.getMinLimit() +
-////			versionRange.getMin().getOriginal() + "," + versionRange.getMax().getOriginal() + 
-////			versionRange.getMaxLimit();
-//		try {
-//		new VersionRange(versionString);
-//		}
-//		catch(IllegalArgumentException e)
-//			{
-//			error("VersionRange '"+versionString+"' is not a valid range: "+e.getMessage(),
-//					versionRange,
-//					BeeLangPackage.VERSION_RANGE);
-//		}
-//	}
+	
 	/**
 	 * A proceed expression can only occur in expressions that are going to be used for weaving.
 	 * These are specified in a BFunctionConcernContext, or a BuilderConcernContext.
