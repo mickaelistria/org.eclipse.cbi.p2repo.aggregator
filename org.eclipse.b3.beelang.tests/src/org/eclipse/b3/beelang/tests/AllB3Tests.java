cc7418d3483418233f6541556a16efeb89ed39d4
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-01-22 16:21:46 +0000

    cross cuts test added to b3 test suite

--- org.eclipse.b3.beelang.tests/src/org/eclipse/b3/beelang/tests/AllB3Tests.java
+++ org.eclipse.b3.beelang.tests/src/org/eclipse/b3/beelang/tests/AllB3Tests.java
@@ -13,6 +13,7 @@ import org.junit.runner.RunWith;
 		"/src-b3/testBuildUnitSyntax.b3", //
 		"/src-b3/testCache.b3", //
 		"/src-b3/testExceptions.b3", //
+		"/src-b3/testFunctionCrossCuts.b3", //
 		"/src-b3/testGuard.b3", //
 		"/src-b3/testHelloWorld.b3", //
 		"/src-b3/testIfThenElse.b3", //
