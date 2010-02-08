e6d510bd0b5945d793ec1dba36bbaa3e61919d55
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-02-08 15:39:40 +0000

    implementation of bequests is now also in place

--- org.eclipse.b3.build/src/org/eclipse/b3/build/build/impl/BuildContextImpl.java
+++ org.eclipse.b3.build/src/org/eclipse/b3/build/build/impl/BuildContextImpl.java
@@ -6,14 +6,22 @@
  */
 package org.eclipse.b3.build.build.impl;
 
+import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.b3.backend.core.B3EngineException;
+import org.eclipse.b3.backend.core.B3WeavingFailedException;
 
+import org.eclipse.b3.backend.evaluator.b3backend.B3JavaImport;
+import org.eclipse.b3.backend.evaluator.b3backend.B3MetaClass;
+import org.eclipse.b3.backend.evaluator.b3backend.B3backendFactory;
+import org.eclipse.b3.backend.evaluator.b3backend.BConcern;
+import org.eclipse.b3.backend.evaluator.b3backend.BExecutionContext;
 import org.eclipse.b3.backend.evaluator.b3backend.IFunction;
 import org.eclipse.b3.backend.evaluator.b3backend.impl.BExecutionContextImpl;
+import org.eclipse.b3.backend.evaluator.typesystem.TypeUtils;
 import org.eclipse.b3.build.build.B3BuildPackage;
 import org.eclipse.b3.build.build.BeeModel;
 import org.eclipse.b3.build.build.BuildContext;
@@ -58,53 +66,69 @@ public class BuildContextImpl extends BExecutionContextImpl implements BuildCont
 
 	/**
 	 * <!-- begin-user-doc -->
+	 * Define a build unit. Any bequested matching advice is applied.
+	 * TODO: SEMANTICS OF REDEFINING A UNIT (All sorts of bad things happen) - now exception is thrown
 	 * <!-- end-user-doc -->
-	 * @generated
-	 */
-	public void defineBuildUnit(BuildUnit unit, boolean isWeaving) throws B3EngineException {
-		// TODO: implement this method
-		// Ensure that you remove @generated or mark it @generated NOT
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * <!-- begin-user-doc -->
-	 * Defines the build unit if it is not already defined.
-	 * TODO: Should probably throw an exception if already defined
-	 * <!-- end-user-doc -->
+	 * @throws IllegalArgumentException if the unit has been defined in this context.
 	 * @generated NOT
 	 */
-	public void defineBuildUnit(BuildUnit unit) throws B3EngineException {
+	public void defineBuildUnit(BuildUnit unit, boolean isWeaving) throws B3EngineException {
 		BuildUnitProxyAdapter p = BuildUnitProxyAdapterFactory.eINSTANCE.adapt(unit);
 		Class<? extends BuildUnit> iface = p.getIface();
-		if(!unitStore.containsKey(iface)) {
-			// add the unit, but check for advice first
-			// TODO: check for advice
-			unitStore.put(iface, unit);
+		if(unitStore.containsKey(iface))
+			throw new IllegalArgumentException("Attempt to redefine a build unit named: "+unit.getName());
+
+		BuildUnit unitToStore = unit;
+		boolean woven = false;
+		// check for advice is not weaving
+		if(!isWeaving) {
+			for(BExecutionContext ctx = this; ctx.getParentContext() != null; ctx = ctx.getParentContext()) {
+				for(BConcern c : ctx.getEffectiveConcerns())
+					try {
+						if(c.evaluateIfMatching(unit, this))
+							woven = true;
+					} catch (Throwable e) {
+						throw new B3WeavingFailedException(e);
+					}
+			}
 		}
+		// do not store original if it was woven
+		if(!woven)
+			unitStore.put(iface, unitToStore);
 	}
 
 	/**
 	 * <!-- begin-user-doc -->
-	 * TODO: UNFINISHED - DEFINE BEEMODEL
+	 * TODO: DOES NOT NEED TO RETURN THE CTX
 	 * <!-- end-user-doc -->
 	 * @throws B3EngineException 
 	 * @generated NOT
 	 */
 	public BuildContext defineBeeModel(BeeModel beeModel) throws B3EngineException {
-		IFunction main = null;
+		
+		// Define all IMPORTS as constants
+		for(Type t : beeModel.getImports()) {
+			if(t instanceof B3JavaImport) {
+				Class<?> x = TypeUtils.getRaw(t);
+				B3MetaClass metaClass = B3backendFactory.eINSTANCE.createB3MetaClass();
+				metaClass.setInstanceClass(x);
+				defineValue(((B3JavaImport) t).getName(), x, metaClass);
+			}
+		}
+		
+		// Define all FUNCTIONS
 		for(IFunction f : beeModel.getFunctions()) {
 			this.defineFunction(f);
 		}
-//		// Define all imports as constants
-//		for(Type t : ((BeeModel) state).getImports()) {
-//			if(t instanceof B3JavaImport) {
-//				Class<?> x = TypeUtils.getRaw(t);
-//				B3MetaClass metaClass = B3backendFactory.eINSTANCE.createB3MetaClass();
-//				metaClass.setInstanceClass(x);
-//				engine.getContext().defineValue(((B3JavaImport) t).getName(), x, metaClass);
-//			}
-//		}
+		
+		// Define all BUILD UNITS (currently only one - could easily have more than one)
+		BuildUnit u = beeModel.getBody();
+		if(u != null)
+			defineBuildUnit(u, false);
+
+		// Concerns, and defined property sets are only used via direct model references,
+		// so these are not defined in the context.
+		
 		return this;
 	}
 	/**
