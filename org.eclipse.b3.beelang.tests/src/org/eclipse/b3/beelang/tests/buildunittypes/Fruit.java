42ee4513a39aa7bb20a16807aa9a8feb1b32aada
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-02-16 13:12:07 +0000

    Added methods to Fruit interface.

--- org.eclipse.b3.beelang.tests/src/org/eclipse/b3/beelang/tests/buildunittypes/Fruit.java
+++ org.eclipse.b3.beelang.tests/src/org/eclipse/b3/beelang/tests/buildunittypes/Fruit.java
@@ -1,11 +1,16 @@
 package org.eclipse.b3.beelang.tests.buildunittypes;
 
 import org.eclipse.b3.build.build.BuildUnit;
+import org.eclipse.b3.build.core.B3BuilderJob;
 
 /**
  * Marker interface for "Fruit" build units.
- *
+ * 
  */
 public interface Fruit extends BuildUnit {
+	public B3BuilderJob peel();
 
+	public B3BuilderJob pulp();
+
+	public B3BuilderJob seeds();
 }
