efcd9392b5f15d65fdb5ad89d277a5726c1aff17
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-02-18 14:33:54 +0000

    Changed B3JavaImport to not print stack trace on class not found - expect faulty import to be handled via validation instead. (Otherwise there are lots of error output when user is typing/saving).

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/B3JavaImportImpl.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/b3backend/impl/B3JavaImportImpl.java
@@ -210,8 +210,8 @@ public class B3JavaImportImpl extends EObjectImpl implements B3JavaImport {
 			try {
 				setType(B3BackendActivator.instance.getBundle().loadClass(getQualifiedName()));
 			} catch (ClassNotFoundException e) {
-				// TODO: Need to handle this, so the "not found" surfaces to the user...
-				e.printStackTrace();
+				// TODO: ?? Need to handle this, so the "not found" surfaces to the user...
+				// Handled via model validation - type can not be null
 			}
 		}
 		return type;
