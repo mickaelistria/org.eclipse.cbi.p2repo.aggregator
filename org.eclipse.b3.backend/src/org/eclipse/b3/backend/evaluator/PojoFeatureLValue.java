d8f05824cd1fb77a900168f6a8e7f5deb1453edd
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-02-27 01:46:10 +0000

    Added support for using plain feature names like "size" in PojoFeatureLValue.

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/PojoFeatureLValue.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/evaluator/PojoFeatureLValue.java
@@ -14,10 +14,13 @@ import org.eclipse.b3.backend.evaluator.typesystem.TypeUtils;
 
 public class PojoFeatureLValue implements LValue {
 	Object lhs;
+
 	Method getter;
+
 	Method setter;
+
 	private String featureName;
-	
+
 	public PojoFeatureLValue(Object lhs, String featureName) {
 		this.lhs = lhs;
 		this.featureName = featureName;
@@ -25,30 +28,51 @@ public class PojoFeatureLValue implements LValue {
 		getter = null;
 		try {
 			getter = lhs.getClass().getMethod(beanFeature);
-		} catch(NoSuchMethodException e) { /* ignore */}
+		}
+		catch(NoSuchMethodException e) { /* ignore */
+		}
 		beanFeature = BackendHelper.getIsGetter(featureName);
 		try {
 			getter = lhs.getClass().getMethod(beanFeature);
-		} catch(NoSuchMethodException e) { /* ignore */}			
-		
+		}
+		catch(NoSuchMethodException e) { /* ignore */
+		}
+		try {
+			getter = lhs.getClass().getMethod(featureName);
+		}
+		catch(NoSuchMethodException e) { /* ignore */
+		}
+
 		beanFeature = BackendHelper.getSetter(featureName);
 		setter = null;
 		if(getter == null)
-			return; 
+			return;
 		try {
 			setter = lhs.getClass().getMethod(beanFeature, getter.getReturnType());
-		} catch(NoSuchMethodException e) { /*ignore */ }
+		}
+		catch(NoSuchMethodException e) { /* ignore */
+		}
 	}
+
 	public Object get() throws B3EngineException {
 		if(getter == null)
 			throw new B3NoSuchFeatureException(featureName);
 		try {
 			return getter.invoke(lhs);
-		} catch (Throwable e) {
+		}
+		catch(Throwable e) {
 			throw new B3NoSuchFeatureException(featureName, e);
 		}
 	}
-	
+
+	public Type getDeclaredType() throws B3EngineException {
+		return TypeUtils.objectify(getter.getGenericReturnType());
+	}
+
+	public boolean isGetable() {
+		return getter != null;
+	}
+
 	public boolean isSettable() {
 		return setter != null;
 	}
@@ -58,18 +82,14 @@ public class PojoFeatureLValue implements LValue {
 			throw new B3NoSuchFeatureException(featureName);
 		try {
 			return setter.invoke(lhs, value);
-		} catch (Throwable e) {
+		}
+		catch(Throwable e) {
 			throw new B3NoSuchFeatureException(featureName, e);
 		}
 	}
-	public Type getDeclaredType() throws B3EngineException {
-		return TypeUtils.objectify(getter.getGenericReturnType());
-	}
+
 	public void setDeclaredType(Type t) throws B3EngineException {
 		throw new B3ImmutableTypeException(lhs, getter.getGenericReturnType(), t);
 	}
-	public boolean isGetable()  {
-		return getter != null;
-	}
-	
+
 }
