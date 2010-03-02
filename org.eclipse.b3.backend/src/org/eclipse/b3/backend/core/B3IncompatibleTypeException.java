2e19adc65061057a4ef841778a75443824cc9ab6
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-03-02 23:12:40 +0000

    Improved exception handling for nested calls. An "function with suitable signature not found" error caused a call higher up in the stack to fail, with the result of testing a static call.

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3IncompatibleTypeException.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3IncompatibleTypeException.java
@@ -7,12 +7,12 @@ import org.eclipse.osgi.util.NLS;
 public class B3IncompatibleTypeException extends B3EngineException {
 
 	private static final long serialVersionUID = 1L;
+
 	public B3IncompatibleTypeException(String paramName, Type expected, Type used) {
-		super(NLS.bind(Messages.B3IncompatibleTypeException_0, 
-				new Object[]{paramName, expected, used}));
+		super(NLS.bind(Messages.B3IncompatibleTypeException_0, new Object[] { paramName, expected.toString(), used }));
 	}
+
 	public B3IncompatibleTypeException(Type expected, Type used) {
-		super(NLS.bind(Messages.B3IncompatibleTypeException_1, 
-				new Object[] {expected, used}));
+		super(NLS.bind(Messages.B3IncompatibleTypeException_1, new Object[] { expected.toString(), used }));
 	}
 }
