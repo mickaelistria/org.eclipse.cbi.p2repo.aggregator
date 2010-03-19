5d0623b972b5b8483874713c30240ecc2947e78e
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-03-19 03:25:48 +0000

    formatting

--- org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3Engine.java
+++ org.eclipse.b3.backend/src/org/eclipse/b3/backend/core/B3Engine.java
@@ -1,6 +1,5 @@
 package org.eclipse.b3.backend.core;
 
-
 import org.eclipse.b3.backend.evaluator.b3backend.B3backendFactory;
 import org.eclipse.b3.backend.evaluator.b3backend.BExecutionContext;
 import org.eclipse.b3.backend.evaluator.b3backend.BInvocationContext;
@@ -12,40 +11,44 @@ import org.eclipse.b3.backend.functions.SystemFunctions;
 
 public class B3Engine {
 	protected BSystemContext systemContext;
+
 	protected BInvocationContext invocationContext;
-	
-	public B3Engine () {
+
+	public B3Engine() {
 		systemContext = B3backendFactory.eINSTANCE.createBSystemContext();
 		invocationContext = B3backendFactory.eINSTANCE.createBInvocationContext();
 		invocationContext.setParentContext(systemContext);
 		try {
-		invocationContext.loadFunctions(ArithmeticFunctions.class);
-		invocationContext.loadFunctions(RelationalFunctions.class);
-		invocationContext.loadFunctions(StringFunctions.class);
-		invocationContext.loadFunctions(SystemFunctions.class);
-		} catch(B3EngineException e) {
+			invocationContext.loadFunctions(ArithmeticFunctions.class);
+			invocationContext.loadFunctions(RelationalFunctions.class);
+			invocationContext.loadFunctions(StringFunctions.class);
+			invocationContext.loadFunctions(SystemFunctions.class);
+		}
+		catch(B3EngineException e) {
 			// should not really happen as this is loading default functions
 			e.printStackTrace();
 		}
 		loadInstanceMethods(); // experimental
 	}
+
 	public BExecutionContext getContext() {
 		return invocationContext;
 	}
-	/** 
+
+	/**
 	 * Experimental code - to be removed
 	 */
 	private void loadInstanceMethods() {
-//		Method m = null;
-//		try {
-//			m = String.class.getMethod("format", String.class, Array.newInstance(Object.class, 0).getClass());
-//		} catch (Throwable e) {
-//			e.printStackTrace();
-//		} 
-//		try {
-//			systemContext.loadFunction(m);
-//		} catch (B3EngineException e) {
-//			e.printStackTrace();
-//		}
+		// Method m = null;
+		// try {
+		// m = String.class.getMethod("format", String.class, Array.newInstance(Object.class, 0).getClass());
+		// } catch (Throwable e) {
+		// e.printStackTrace();
+		// }
+		// try {
+		// systemContext.loadFunction(m);
+		// } catch (B3EngineException e) {
+		// e.printStackTrace();
+		// }
 	}
 }
