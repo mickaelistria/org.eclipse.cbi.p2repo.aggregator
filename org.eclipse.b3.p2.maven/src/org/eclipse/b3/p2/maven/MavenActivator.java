65b6df41f34c48eae076b8b07b536c2feaa5a6c4
Filip Hrbek <filip.hrbek@cloudsmith.com>
2010-04-15 12:41:24 +0000

    Initial check-in

---
+++ org.eclipse.b3.p2.maven/src/org/eclipse/b3/p2/maven/MavenActivator.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2006-2007, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ ******************************************************************************/
+package org.eclipse.b3.p2.maven;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URI;
+
+import org.eclipse.b3.p2.maven.util.DigestUtil;
+import org.eclipse.core.runtime.Plugin;
+import org.eclipse.equinox.internal.p2.core.AgentLocation;
+import org.eclipse.equinox.internal.p2.core.helpers.ServiceHelper;
+import org.eclipse.equinox.internal.p2.core.helpers.URLUtil;
+import org.eclipse.equinox.p2.core.IAgentLocation;
+import org.osgi.framework.BundleContext;
+
+public class MavenActivator extends Plugin {
+	public static final String ID = "org.eclipse.b3.p2.maven";
+
+	public static MavenActivator plugin;
+
+	public static MavenActivator getPlugin() {
+		return plugin;
+	}
+
+	private AgentLocation agentLocation;
+
+	public MavenActivator() {
+		plugin = this;
+	}
+
+	public File getCacheDirectory(URI location) throws MalformedURLException {
+		URI dataArea = agentLocation.getDataArea(ID + "/cache/" + encodeURI(location)); //$NON-NLS-1$
+		return URLUtil.toFile(dataArea.toURL());
+	}
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		agentLocation = (AgentLocation) ServiceHelper.getService(context, IAgentLocation.SERVICE_NAME);
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+	}
+
+	private String encodeURI(URI location) {
+		return DigestUtil.encodeMD5(location.toString());
+	}
+}
---
+++ org.eclipse.b3.p2.maven/src/org/eclipse/b3/p2/maven/util/DigestUtil.java
@@ -0,0 +1,75 @@
+/**
+ * Copyright (c) 2006-2009, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ */
+
+package org.eclipse.b3.p2.maven.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * @author filip.hrbek@cloudsmith.com
+ * 
+ */
+public class DigestUtil {
+	public static final MessageDigest[] MESSAGE_DIGESTERS;
+
+	static {
+		String[] algorithms = { "MD5", "SHA1" };
+		MESSAGE_DIGESTERS = new MessageDigest[algorithms.length];
+		int i = 0;
+		for(String checkSumAlgorithm : algorithms)
+			try {
+				MESSAGE_DIGESTERS[i++] = MessageDigest.getInstance(checkSumAlgorithm);
+			}
+			catch(NoSuchAlgorithmException e) {
+				throw new RuntimeException("Unable to create checksum algorithm for " + checkSumAlgorithm + ": "
+						+ e.getMessage());
+			}
+	}
+
+	public static String[] createCheckSum(byte[] content, MessageDigest[] digests) {
+		String[] result = new String[digests.length];
+
+		int i = 0;
+		StringBuilder checkSumStr = new StringBuilder(32);
+		for(MessageDigest digest : digests) {
+			digest.reset();
+			byte[] checkSum = digest.digest(content);
+			checkSumStr.setLength(0);
+			for(byte b : checkSum)
+				checkSumStr.append(String.format("%02x", Byte.valueOf(b)));
+
+			result[i++] = checkSumStr.toString();
+		}
+
+		return result;
+	}
+
+	public static String encodeMD5(String str) {
+		return encode(str, 0);
+	}
+
+	public static String encodeSHA1(String str) {
+		return encode(str, 1);
+	}
+
+	private static String encode(String str, int algorithmIndex) {
+		byte[] digest = MESSAGE_DIGESTERS[algorithmIndex].digest(str.getBytes());
+		return formatDigest(digest);
+	}
+
+	private static String formatDigest(byte[] digest) {
+		StringBuilder result = new StringBuilder(digest.length << 1);
+
+		for(byte b : digest)
+			result.append(String.format("%02x", Byte.valueOf(b)));
+
+		return result.toString();
+	}
+
+}
