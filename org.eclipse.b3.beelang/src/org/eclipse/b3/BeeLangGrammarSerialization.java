18d35b6f6deff1816457d928dda42d9c8eeaa356
Henrik Lindberg <henrik.lindberg@cloudsmith.com>
2010-05-11 14:01:41 +0000

    Serialization issue causing quick fix to fail repaired.

--- org.eclipse.b3.beelang/src/org/eclipse/b3/BeeLangGrammarSerialization.java
+++ org.eclipse.b3.beelang/src/org/eclipse/b3/BeeLangGrammarSerialization.java
@@ -1,33 +1,39 @@
 package org.eclipse.b3;
 
 import org.eclipse.b3.build.build.B3BuildPackage;
-import org.eclipse.b3.build.build.BuildUnit;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
-import org.eclipse.xtext.parsetree.reconstr.ITransientValueService;
+import org.eclipse.xtext.parsetree.reconstr.impl.DefaultTransientValueService;
 
-public class BeeLangGrammarSerialization implements ITransientValueService {
+public class BeeLangGrammarSerialization extends DefaultTransientValueService {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.xtext.parsetree.reconstr.ITransientValueService#isCheckElementsIndividually(org.eclipse.emf.ecore.EObject,
-	 * org.eclipse.emf.ecore.EStructuralFeature)
-	 */
-	public boolean isCheckElementsIndividually(EObject owner, EStructuralFeature feature) {
-		return true;
-	}
+	// /*
+	// * (non-Javadoc)
+	// *
+	// * @see org.eclipse.xtext.parsetree.reconstr.ITransientValueService#isCheckElementsIndividually(org.eclipse.emf.ecore.EObject,
+	// * org.eclipse.emf.ecore.EStructuralFeature)
+	// */
+	// public boolean isCheckElementsIndividually(EObject owner, EStructuralFeature feature) {
+	// return true;
+	// }
 
 	// public boolean isMixedList(EObject owner, EStructuralFeature feature) {
 	// return true;
 	// }
 
+	// public boolean isTransient(EObject owner, EStructuralFeature feature, int index) {
+	// if(!(owner instanceof BuildUnit))
+	// return false;
+	// if(feature.getFeatureID() == B3BuildPackage.BUILD_UNIT__NAME_SPACE)
+	// return true;
+	// return false;
+	// }
+	@Override
 	public boolean isTransient(EObject owner, EStructuralFeature feature, int index) {
-		if(!(owner instanceof BuildUnit))
-			return false;
-		if(feature.getFeatureID() == B3BuildPackage.BUILD_UNIT__NAME_SPACE)
+		if(owner.eClass() == B3BuildPackage.Literals.BUILD_UNIT &&
+				feature == B3BuildPackage.Literals.CAPABILITY__NAME_SPACE) {
 			return true;
-		return false;
+		}
+		return super.isTransient(owner, feature, index);
 	}
-
 }
