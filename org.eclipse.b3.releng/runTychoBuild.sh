1a049c0cd12438e84d2407f5a81a3478ad8e89f7
David Williams <david_williams@acm.org>
2016-10-20 00:05:09 -0400

    [releng] improved script (test for Gerrit, again)
    Change-Id: I8186b15d8e2edd7b408217d88d24f9cc0500f2dd
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng/runTychoBuild.sh
+++ org.eclipse.b3.releng/runTychoBuild.sh
@@ -1,48 +1,49 @@
 #!/usr/bin/env bash
 
-BUILD_HOME=/home/davidw/gitCBI/
-export TMP_DIR=${BUILD_HOME}/aggr/TMP
-export LOCAL_REPO=${BUILD_HOME}/aggr/LOCALMVNREPO
+build_home=${WORKSPACE:-/home/davidw/gitCBI/}
+export tmp_dir=${build_home}/aggr/tmp
+export local_mvn_repo=${build_home}/aggr/localMvnRepo
 
-REPO_DIR=${BUILD_HOME}/org.eclipse.cbi.p2repo.aggregator
-export POM_DIR=${REPO_DIR}/org.eclipse.b3.releng.parent
+repo_dir=${build_home}/org.eclipse.cbi.p2repo.aggregator
+export pom_dir=${repo_dir}/org.eclipse.b3.releng.parent
 
-out_file=${BUILD_HOME}/aggr/buildOutput.txt
+out_file=${build_home}/aggr/buildOutput.txt
 
 # This first write does not "append", the rest need to.
 env | tee ${out_file}
 
+cleanLocal=$1
 
-if [[ $1 == -clean ]]
+if [[ "${cleanLocal}" == "true" ]]
 then
-  rm -fr ${TMP_DIR}
-  rm -fr ${LOCAL_REPO}
-  printf "\n\t[INFO] %s\n\n" "-clean argument was specified on command line, so tmp and local maven repo were removed" | tee -a ${out_file}
+  rm -fr ${tmp_dir}
+  rm -fr ${local_mvn_repo}
+  printf "\n\t[INFO] %s\n\n" "cleanLocal argument was true so tmp and local maven repo were removed" | tee -a ${out_file}
 else
-  printf "\n\t[WARNING] %s\n\n" "No -clean argument was specified on command line, so tmp and local maven repo were not removed" | tee -a ${out_file}
+  printf "\n\t[WARNING] %s\n\n" "cleanLocal argument was not true so tmp and local maven repo were not removed" | tee -a ${out_file}
 fi
-mkdir -p ${TMP_DIR}
-mkdir -p ${LOCAL_REPO}
+mkdir -p ${tmp_dir}
+mkdir -p ${local_mvn_repo}
 
 # Without the ANT_OPTS, we do get messages about "to get repeatable builds, to ignore sysclasspath"
 export ANT_HOME=${ANT_HOME:-/shared/common/apache-ant-1.9.6}
 export ANT_OPTS=${ANT_OPTS:-"-Dbuild.sysclasspath=ignore -Dincludeantruntime=false"}
-export MAVEN_OPTS=${MAVEN_OPTS:--Xms1048m -Xmx2096m -Djava.io.tmpdir=${TMP_DIR} -Dtycho.localArtifacts=ignore ${MIRROR_SETTING}}
+export MAVEN_OPTS=${MAVEN_OPTS:--Xms1048m -Xmx2096m -Djava.io.tmpdir=${tmp_dir} -Dtycho.localArtifacts=ignore ${MIRROR_SETTING}}
 export JAVA_HOME=/shared/common/jdk1.8.0_x64-latest
 export JAVA_CMD=${JAVA_HOME}/jre/bin/java
 export MAVEN_PATH=${MAVEN_PATH:-/shared/common/apache-maven-latest/bin}
 export PATH=$JAVA_HOME/bin:$MAVEN_PATH:$ANT_HOME/bin:$PATH
 
-cd ${POM_DIR}
+cd ${pom_dir}
 
-printf "\n\t[INFO] %s\n\n" "Maven Version:" | tee -a ${out_file}
-mvn -version 2>&1 | tee -a ${out_file}
+#printf "\n\t[INFO] %s\n\n" "Maven Version:" | tee -a ${out_file}
+#mvn -version 2>&1 | tee -a ${out_file}
 
-printf "\n\t[INFO] %s\n\n" "Java Version:" | tee -a ${out_file}
-java -version 2>&1 | tee -a ${out_file}
+#printf "\n\t[INFO] %s\n\n" "Java Version:" | tee -a ${out_file}
+#java -version 2>&1 | tee -a ${out_file}
 
 printf "\n\t[INFO] %s\n\n" "mvn clean verify ...:" | tee -a ${out_file}
-mvn clean verify -X -e -DskipTests=true -Dmaven.repo.local=$LOCAL_REPO -Pbree-libs  2>&1 | tee -a ${out_file}
+mvn clean verify -X -e -DskipTests=true -Dmaven.repo.local=$local_mvn_repo -Pbree-libs  2>&1 | tee -a ${out_file}
 
 printf "\n\t[INFO] %s\n\n" "Build Output in ${out_file}"
 
