b69d19a59702b664b54b575ba888fe6d2d8372ba
David Williams <david_williams@acm.org>
2016-10-19 22:25:07 -0400

    [releng] convenience file
    Change-Id: Ia58a6e61ee23dcd631b8a5d8e5ad616125935f9b
    Signed-off-by: David Williams <david_williams@acm.org>
    
    
    Change-Id: Ia58a6e61ee23dcd631b8a5d8e5ad616125935f9b
    Signed-off-by: David Williams <david_williams@acm.org>

---
+++ org.eclipse.b3.releng/miscUtilities/runProductSimRel.sh
@@ -0,0 +1,2 @@
+git clone ssh://david_williams@git.eclipse.org:29418/simrel/org.eclipse.simrel.build
+./eclipse aggregate --action BUILD --logLevel DEBUG --buildModel org.eclipse.simrel.build/simrel.b3aggr
