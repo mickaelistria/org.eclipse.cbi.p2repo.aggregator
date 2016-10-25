13e4a5e9f63ecfcc7dc8c7baa81bdf710506bbd8
David Williams <david_williams@acm.org>
2016-10-25 07:40:20 -0400

    Bug 487478 - correct and simplify build
    Change-Id: Iaf3c17eb2ff38950ebd29053b585a6e709f4a4a2
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng/runTychoBuild.sh
+++ org.eclipse.b3.releng/runTychoBuild.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
-build_home=${WORKSPACE:-/home/davidw/gitCBI/}
+build_home=${WORKSPACE:-/home/davidw/gitCBI}
 export tmp_dir=${build_home}/aggr/tmp
 export local_mvn_repo=${build_home}/aggr/localMvnRepo
 
 repo_dir=${build_home}/org.eclipse.cbi.p2repo.aggregator
-export pom_dir=${repo_dir}/org.eclipse.b3.releng.parent
 
 out_file=${build_home}/aggr/buildOutput.txt
 
@@ -34,13 +33,7 @@ export JAVA_CMD=${JAVA_HOME}/jre/bin/java
 export MAVEN_PATH=${MAVEN_PATH:-/shared/common/apache-maven-latest/bin}
 export PATH=$JAVA_HOME/bin:$MAVEN_PATH:$ANT_HOME/bin:$PATH
 
-cd ${pom_dir}
-
-#printf "\n\t[INFO] %s\n\n" "Maven Version:" | tee -a ${out_file}
-#mvn -version 2>&1 | tee -a ${out_file}
-
-#printf "\n\t[INFO] %s\n\n" "Java Version:" | tee -a ${out_file}
-#java -version 2>&1 | tee -a ${out_file}
+cd ${repo_dir}
 
 printf "\n\t[INFO] %s\n\n" "mvn clean verify ...:" | tee -a ${out_file}
 mvn clean verify -X -e -DskipTests=true -Dmaven.repo.local=$local_mvn_repo -Pbree-libs  2>&1 | tee -a ${out_file}
