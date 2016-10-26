c6d9a9005e7e8c8c273ec9247ee948c8928e36f6
David Williams <david_williams@acm.org>
2016-10-26 15:38:34 -0400

    Bug 487478 - correct (and fine tune) some properties
    Change-Id: If478866156d840b575b3c7dd8ee95e172a781f4b
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng/runTychoBuild.sh
+++ org.eclipse.b3.releng/runTychoBuild.sh
@@ -34,7 +34,8 @@ fi
 # Without the ANT_OPTS, we do get messages about "to get repeatable builds, to ignore sysclasspath"
 export ANT_HOME=${ANT_HOME:-/shared/common/apache-ant-1.9.6}
 export ANT_OPTS=${ANT_OPTS:-"-Dbuild.sysclasspath=ignore -Dincludeantruntime=false"}
-export MAVEN_OPTS=${MAVEN_OPTS:--Xms1048m -Xmx2096m -Djava.io.tmpdir=${tmp_dir} -Dtycho.localArtifacts=ignore ${MIRROR_SETTING} ${DIRTY_SETTING}}
+export MAVEN_OPTS="-Xms1048m -Xmx2096m -Djava.io.tmpdir=${tmp_dir} ${MIRROR_SETTING}"
+TYCHO_ARGS="-Djava.io.tmpdir=${tmp_dir} -Dtycho.localArtifacts=ignore -Dtycho.debug.artifactcomparator ${MIRROR_SETTING} ${DIRTY_SETTING}"
 export JAVA_HOME=/shared/common/jdk1.8.0_x64-latest
 export JAVA_CMD=${JAVA_HOME}/jre/bin/java
 export MAVEN_PATH=${MAVEN_PATH:-/shared/common/apache-maven-latest/bin}
@@ -42,8 +43,11 @@ export PATH=$JAVA_HOME/bin:$MAVEN_PATH:$ANT_HOME/bin:$PATH
 
 cd ${repo_dir}
 
-printf "\n\t[INFO] %s\n\n" "mvn clean verify ...:" | tee -a ${out_file}
-mvn clean verify -X -e -DskipTests=true -Dmaven.repo.local=$local_mvn_repo -Pbree-libs ${sign_jars_arg} 2>&1 | tee -a ${out_file}
+printf "\n\t[INFO] %s\n" "mvn clean verify ...:" | tee -a ${out_file}
+printf "\n\t[INFO] %s\n" "MAVEN_OPTS: ${MAVEN_OPTS}" | tee -a ${out_file}
+printf "\n\t[INFO] %s\n\n" "TYCHO_ARGS: ${TYCHO_ARGS}" | tee -a ${out_file}
+# All -D type settings need to come before the -P settings, apparently.
+mvn clean verify -X -e -DskipTests=true -Dmaven.repo.local=$local_mvn_repo ${TYCHO_ARGS} -Pbree-libs ${sign_jars_arg} 2>&1 | tee -a ${out_file}
 RC=$?
 
 printf "\n\t[INFO] %s\n\n" "Build Output in ${out_file}"
