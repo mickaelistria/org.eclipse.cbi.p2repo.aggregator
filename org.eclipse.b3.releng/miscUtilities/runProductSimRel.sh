a5b8be2444cf8c2218ee8176c1434c88ff869c9e
David Williams <david_williams@acm.org>
2016-10-30 00:32:16 -0400

    487478 Removed releng project
    Change-Id: I28247cccfd69836f6b54ad31e2de1bd1383cf2b3
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng/miscUtilities/runProductSimRel.sh
+++
@@ -1,2 +0,0 @@
-git clone ssh://david_williams@git.eclipse.org:29418/simrel/org.eclipse.simrel.build
-./eclipse aggregate --action BUILD --logLevel DEBUG --buildModel org.eclipse.simrel.build/simrel.b3aggr
