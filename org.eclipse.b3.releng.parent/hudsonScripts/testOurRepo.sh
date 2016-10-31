1e91f391cd5fc50012954bf7f72d1bb25b6713f7
David Williams <david_williams@acm.org>
2016-10-30 20:07:39 -0400

    [releng] improve variables written for deploy and testing.
    Change-Id: I32634060e372e11c37ff54659240534eb8b70d20
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng.parent/hudsonScripts/testOurRepo.sh
+++ org.eclipse.b3.releng.parent/hudsonScripts/testOurRepo.sh
@@ -22,9 +22,9 @@ mkdir -p ${build_home}/testarea
 pushd ${build_home}/testarea
 wget -O org.eclipse.cbi.p2repo.analyzers.product-linux.gtk.x86_64.tar.gz  https://hudson.eclipse.org/cbi/job/cbi.p2repo.analyzers.build/lastSuccessfulBuild/artifact/output/products/org.eclipse.cbi.p2repo.analyzers.product-linux.gtk.x86_64.tar.gz
 tar -xf org.eclipse.cbi.p2repo.analyzers.product-linux.gtk.x86_64.tar.gz
-
-unzip ${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.b3.site.eclipse/target/org.eclipse.b3.site.eclipse-1.0.0-SNAPSHOT.zip -d repoToTest
 popd
+
+unzip ${build_home}/org.eclipse.cbi.p2repo.aggregator/org.eclipse.b3.site.eclipse/target/org.eclipse.b3.site.eclipse-1.0.0-SNAPSHOT.zip -d ${build_home}/testarea/repoToTest
 # TODO: it is very awkward to have to update referenceRepo (in parent pom) so frequently 
 source $sourceProperties
 ${build_home}/testarea/p2analyze -data workspace-report -vmargs -DreportRepoDir=${build_home}/testarea/repoToTest -DreferenceRepo=${referenceRepo}  -DreportOutputDir=${build_home}/p2repoSelfReport 2>&1 | tee -a ${out_file}
