4e1c70354058f2f8c999e4d18424f6aa8db62465
David Williams <david_williams@acm.org>
2016-11-07 16:36:19 -0500

    Bug 507119 - Build aggregator against Neon.1
    Change "final repo" where composite it made to "4.6".
    
    Change-Id: I6edb33bf0ab7dc3b40189be1fb92c6debb62d58b
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.b3.releng.parent/hudsonScripts/writeComposites.sh
+++ org.eclipse.b3.releng.parent/hudsonScripts/writeComposites.sh
@@ -63,7 +63,7 @@ function writeChildren
 
 }
 
-repoRoots=("/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/ide/4.5" "/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/headless/4.5")
+repoRoots=("/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/ide/4.6" "/home/data/httpd/download.eclipse.org/cbi/updates/aggregator/headless/4.6")
 # Normally "writeRepoRoots" is the same as "repoRoots", but might not always be, plus
 # it is very handy for testing this script not to have to write to the "production" area.
 #writeRepoRoots=("${PWD}/ide" "${PWD}/headless")
