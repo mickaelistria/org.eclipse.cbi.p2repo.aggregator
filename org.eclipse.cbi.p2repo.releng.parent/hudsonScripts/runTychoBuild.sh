8456c31a446cc89beb88c004636bb942fc38be39
David Williams <david_williams@acm.org>
2016-12-14 15:50:42 -0500

    Bug 509176 - create a proper "download page" for aggregator
    Corrected some "build log" issues.
    
    Change-Id: I9f909f1eae313b0c6a6d84a4ff6e4871d07088af
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.cbi.p2repo.releng.parent/hudsonScripts/runTychoBuild.sh
+++ org.eclipse.cbi.p2repo.releng.parent/hudsonScripts/runTychoBuild.sh
@@ -11,10 +11,10 @@ out_file=${build_home}/aggr/buildOutput.txt
 # This first write does not "append", the rest need to.
 env | tee ${out_file}
 
-echo -e "\n\t[INFO] all args passed to ${0##*/}:" >> ${out_file}
+echo -e "\n\t[INFO] all args passed to ${0##*/}:"| tee -a ${out_file}
 for arg in "$@" 
 do
-    echo -e "\t\t>$arg<"  >> ${out_file}
+    echo -e "\t\t>$arg<"  | tee -a ${out_file}
 done
 echo -e "\n"
 
@@ -67,14 +67,14 @@ printf "\n\t[INFO] %s\n\n" "TYCHO_ARGS: ${TYCHO_ARGS}" | tee -a ${out_file}
 # All -D type settings need to come before the -P settings, apparently.
 # Note: signJars and cleanLocal values are passed in as -D values simply so they can be captured in 
 #       the "saveproperties/mavenproperties.properties" file.
-mvn clean verify -X -e --update-snapshots -DsignJars=${signJars} -DcleanLocal=${cleanLocal} -DskipTests=true -Dmaven.repo.local=$local_mvn_repo ${TYCHO_ARGS} -Pbree-libs ${sign_jars_arg} 2>&1 | tee -a ${out_file}
+mvn clean verify -X -e --update-snapshots -DsignJars=${signJars} -DcleanLocal=${cleanLocal} -DskipTests=true -Dmaven.repo.local=$local_mvn_repo ${TYCHO_ARGS} -Pbree-libs ${sign_jars_arg} 2>>&1 | tee -a ${out_file}
 RC=${PIPESTATUS[0]}
 
 printf "\n\t[INFO] %s\n\n" "Build Output in ${out_file}"
 
 if [[ $RC != 0 ]]
 then
-  echo -e "[ERROR] mvn returned a non-zero error code: $RC. Exiting.\n"
+  echo -e "[ERROR] mvn returned a non-zero error code: $RC. Exiting.\n" | tee -a ${out_file}
   exit $RC
 fi
 
