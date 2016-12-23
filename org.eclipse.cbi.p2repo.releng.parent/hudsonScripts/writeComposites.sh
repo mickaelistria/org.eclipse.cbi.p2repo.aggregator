95495cd0d91f05d8f7788cbe24bff50e36426d62
David Williams <david_williams@acm.org>
2016-12-23 13:44:26 -0500

    Bug 509176 - create a proper "download page" for aggregator
    Being more explicit and more checking
    
    Change-Id: I62b22348da182b83ec4ff9a724e70888cdf4fb53
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.cbi.p2repo.releng.parent/hudsonScripts/writeComposites.sh
+++ org.eclipse.cbi.p2repo.releng.parent/hudsonScripts/writeComposites.sh
@@ -2,79 +2,79 @@
 
 function writeArtifactsHeader
 {
-    outfile=$1
-    printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
-    printf "%s\n" "<?compositeArtifactRepository version='1.0.0'?>" >> ${outfile}
-    printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>" >> ${outfile}
-    printf "%s\n" "  <properties size='3'>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
-    printf "%s\n" "  </properties>" >> ${outfile}
-    printf "%s\n" "  <children size='3'>" >> ${outfile}
+  outfile=$1
+  printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
+  printf "%s\n" "<?compositeArtifactRepository version='1.0.0'?>" >> ${outfile}
+  printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.artifact.repository.CompositeArtifactRepository' version='1.0.0'>" >> ${outfile}
+  printf "%s\n" "  <properties size='3'>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
+  printf "%s\n" "  </properties>" >> ${outfile}
+  printf "%s\n" "  <children size='3'>" >> ${outfile}
 
 }
 
 function writeContentHeader
 {
-    outfile=$1
-    printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
-    printf "%s\n" "<?compositeMetadataRepository version='1.0.0'?>" >> ${outfile}
-    printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>" >> ${outfile}
-    printf "%s\n" "  <properties size='3'>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
-    printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
-    printf "%s\n" "  </properties>" >> ${outfile}
-    printf "%s\n" "  <children size='3'>" >> ${outfile}
+  outfile=$1
+  printf "%s\n" "<?xml version='1.0' encoding='UTF-8'?>" > ${outfile}
+  printf "%s\n" "<?compositeMetadataRepository version='1.0.0'?>" >> ${outfile}
+  printf "%s\n" "<repository name='Eclipse CBI p2 Repository Aggregator'  type='org.eclipse.equinox.internal.p2.metadata.repository.CompositeMetadataRepository' version='1.0.0'>" >> ${outfile}
+  printf "%s\n" "  <properties size='3'>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.timestamp' value='1313779613118'/>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.compressed' value='true'/>" >> ${outfile}
+  printf "%s\n" "    <property name='p2.atomic.composite.loading' value='true'/>" >> ${outfile}
+  printf "%s\n" "  </properties>" >> ${outfile}
+  printf "%s\n" "  <children size='3'>" >> ${outfile}
 
 }
 
 function writeFooter
 {
-    outfile=$1
-    printf "%s\n" "  </children>" >> ${outfile}
-    printf "%s\n" "</repository>" >> ${outfile}
+  outfile=$1
+  printf "%s\n" "  </children>" >> ${outfile}
+  printf "%s\n" "</repository>" >> ${outfile}
 }
 
 function writeCompositeP2Index
 {
-    printf "%s\n" "version=1" > "${p2Index}"
-    printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${p2Index}"
-    printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${p2Index}"
+  printf "%s\n" "version=1" > "${p2Index}"
+  printf "%s\n" "metadata.repository.factory.order=compositeContent.xml" >> "${p2Index}"
+  printf "%s\n" "artifact.repository.factory.order=compositeArtifacts.xml" >> "${p2Index}"
 }
 
 function writeChildren
 {
-    outfile=$1
-    repoRoot=$2
-    # NOTE: we always take "most recent 3 builds".
-    # we use "I20" as prefix that all our child repo directories start with
-    # such as "I2016...". So, in 80 years will need some maintenance. :)
-    # But, otherwise, this cheap heuristic finds existing files such as "composite*".
-    pushd "${repoRoot}" >/dev/null
-    children=$(ls -1td I20* | head -3)
-    popd >/dev/null
+  outfile=$1
+  repoRoot=$2
+  # NOTE: we always take "most recent 3 builds".
+  # we use "I20" as prefix that all our child repo directories start with
+  # such as "I2016...". So, in 80 years will need some maintenance. :)
+  # But, otherwise, this cheap heuristic finds existing files such as "composite*".
+  pushd "${repoRoot}" >/dev/null
+  children=$(ls -1td I20* | head -3)
+  popd >/dev/null
 
-    for child in $children
-    do
-        printf "%s%s%s\n" "    <child location='" $child "' />"  >> ${outfile}
-    done
+  for child in $children
+  do
+    printf "%s%s%s\n" "    <child location='" $child "' />"  >> ${outfile}
+  done
 
 }
 
 function getLatestBuildId
 {
-    repoRoot=$1
-    # NOTE: we always take "most recent 3 builds".
-    # we use "I20" as prefix that all our child repo directories start with
-    # such as "I2016...". So, in 80 years will need some maintenance. :)
-    # But, otherwise, this cheap heuristic finds existing files such as "composite*".
-    pushd "${repoRoot}" >/dev/null
-    latest=$(ls -1td I20* | head -1)
-    popd >/dev/null
+  repoRoot=$1
+  # NOTE: we always take "most recent 3 builds".
+  # we use "I20" as prefix that all our child repo directories start with
+  # such as "I2016...". So, in 80 years will need some maintenance. :)
+  # But, otherwise, this cheap heuristic finds existing files such as "composite*".
+  pushd "${repoRoot}" >/dev/null
+  latest=$(ls -1td I20* | head -1)
+  popd >/dev/null
 
-    echo "${latest[0]}"
+  echo "${latest[0]}"
 
 }
 
@@ -86,32 +86,32 @@ writeRepoRoots=(${repoRoots[@]})
 indices=(0 1)
 for index in ${indices[@]}
 do
-    #echo -e "[DEBUG] index: ${index}\n"
-    writeRepoRoot="${writeRepoRoots[$index]}"
-    #echo -e "[DEBUG] writeRepoRoot: ${writeRepoRoot}\n"
-    mkdir -p "${writeRepoRoot}"
-    RC=$?
-    if [[ $RC != 0 ]]
-    then
-       echo -e "[ERROR] Could not create directory at ${writeRepoRoot}\n"
-       exit $RC
-    fi
-    repoRoot="${repoRoots[$index]}"
-    #echo -e "[DEBUG] repoRoot: ${repoRoot}\n"
+  #echo -e "[DEBUG] index: ${index}\n"
+  writeRepoRoot="${writeRepoRoots[$index]}"
+  #echo -e "[DEBUG] writeRepoRoot: ${writeRepoRoot}\n"
+  mkdir -p "${writeRepoRoot}"
+  RC=$?
+  if [[ $RC != 0 ]]
+  then
+    echo -e "[ERROR] Could not create directory at ${writeRepoRoot}\n"
+    exit $RC
+  fi
+  repoRoot="${repoRoots[$index]}"
+  #echo -e "[DEBUG] repoRoot: ${repoRoot}\n"
 
-    artifactsCompositeFile="${writeRepoRoot}/compositeArtifacts.xml"
-    contentCompositeFile="${writeRepoRoot}/compositeContent.xml"
-    p2Index="${writeRepoRoot}/p2.index"
+  artifactsCompositeFile="${writeRepoRoot}/compositeArtifacts.xml"
+  contentCompositeFile="${writeRepoRoot}/compositeContent.xml"
+  p2Index="${writeRepoRoot}/p2.index"
 
-    writeArtifactsHeader "${artifactsCompositeFile}"
-    writeChildren "${artifactsCompositeFile}" "${repoRoot}"
-    writeFooter "${artifactsCompositeFile}"
+  writeArtifactsHeader "${artifactsCompositeFile}"
+  writeChildren "${artifactsCompositeFile}" "${repoRoot}"
+  writeFooter "${artifactsCompositeFile}"
 
-    writeContentHeader "${contentCompositeFile}"
-    writeChildren "${contentCompositeFile}" "${repoRoot}"
-    writeFooter "${contentCompositeFile}"
+  writeContentHeader "${contentCompositeFile}"
+  writeChildren "${contentCompositeFile}" "${repoRoot}"
+  writeFooter "${contentCompositeFile}"
 
-    writeCompositeP2Index "${p2Index}"
+  writeCompositeP2Index "${p2Index}"
 done
 
 # Now write latest ${buildId}/buildResults.html to our web site/index.html
@@ -123,6 +123,10 @@ done
 # ${WORKSPACE}/cbi/downloads/aggregatorLatest/
 # but then we still need to commit and push (under the cbi.genie Id)
 # reporoots[1] should be the "headless" repository.
+
+printf "\n\t[INFO] %s\n" "Confirm \$USER and 'whoami' with git config"
+printf "\n\t[INFO] %s\n" "\$USER is $USER"
+printf "\n\t[INFO] %s\n" "whoami is $(whoami)"
 
 pushd ${WORKSPACE}/cbi
 git config --global push.default simple
@@ -137,13 +141,13 @@ git pull
 RC=$?
 if [[ $RC != 0 ]]
 then
-    printf "\n\t[WARNING] %s\n" "git pull returned non-zero return code: $RC. Will try reset."
-    git reset --hard
-    RC=$?
-    if [[ $RC != 0 ]]
-    then
-        printf "\n\t[WARNING] %s\n" "git reset --hard returned non-zero return code: $RC. Will try to continue ..."
-    fi
+  printf "\n\t[WARNING] %s\n" "git pull returned non-zero return code: $RC. Will try reset."
+  git reset --hard
+  RC=$?
+  if [[ $RC != 0 ]]
+  then
+    printf "\n\t[WARNING] %s\n" "git reset --hard returned non-zero return code: $RC. Will try to continue ..."
+  fi
 fi
 printf "\n\t[INFO] %s\n" " = = Confirm status is clear = ="
 git status
@@ -155,10 +159,10 @@ cp ${repoRoots[1]}/${latestBuildId}/buildResults.html ${WORKSPACE}/cbi/downloads
 RC=$?
 if [[ $RC != 0 ]]
 then
-    printf "\n\t[ERROR] %s\n" "Copy to index.html returned non-zero return code: $RC. Exiting early."
-    exit $RC
+  printf "\n\t[ERROR] %s\n" "Copy to index.html returned non-zero return code: $RC. Exiting early."
+  exit $RC
 else
-    printf "\n\t[INFO] %s\n" "Copied ${latestBuildId}/buildResults.html to aggregatorLatest/index.html"
+  printf "\n\t[INFO] %s\n" "Copied ${latestBuildId}/buildResults.html to aggregatorLatest/index.html"
 fi
 
 printf "\n\t[INFO] %s\n" " = = Confirm status shows one change = ="
@@ -169,16 +173,16 @@ git commit --verbose -a -m "Auto commit from Hudson 'cbi.p2repo.aggregator_addCo
 RC=$?
 if [[ $RC != 0 ]]
 then
-    printf "\n\t[ERROR] %s\n" "git commit returned non-zero return code: $RC. Exiting early."
-    exit $RC
+  printf "\n\t[ERROR] %s\n" "git commit returned non-zero return code: $RC. Exiting early."
+  exit $RC
 fi
 # 'origin1' is the name Hudson has assigned
 git push --verbose origin1 HEAD:refs/heads/master
 RC=$?
 if [[ $RC != 0 ]]
 then
-    printf "\n\t[ERROR] %s\n" "git push returned non-zero return code: $RC. Exiting early."
-    exit $RC
+  printf "\n\t[ERROR] %s\n" "git push returned non-zero return code: $RC. Exiting early."
+  exit $RC
 fi
 
 popd
