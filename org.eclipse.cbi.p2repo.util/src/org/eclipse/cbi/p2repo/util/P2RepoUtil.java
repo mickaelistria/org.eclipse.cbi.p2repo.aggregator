6d2026171aaca9dab81eb881d62388cf808c5092
David Williams <david_williams@acm.org>
2017-03-11 15:33:20 -0500

    [releng] Minor fix to put copyright at top of file.
    Change-Id: Ia946e94796551d64ca2b33ffc9031d6bd9d5715c
    Signed-off-by: David Williams <david_williams@acm.org>

--- org.eclipse.cbi.p2repo.util/src/org/eclipse/cbi/p2repo/util/P2RepoUtil.java
+++ org.eclipse.cbi.p2repo.util/src/org/eclipse/cbi/p2repo/util/P2RepoUtil.java
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2006-2009, Cloudsmith Inc.
+ * The code, documentation and other materials contained herein have been
+ * licensed under the Eclipse Public License - v 1.0 by the copyright holder
+ * listed above, as the Initial Contributor under such license. The text of
+ * such license is available at www.eclipse.org.
+ * @author filip.hrbek@cloudsmith.com
+ */
 package org.eclipse.cbi.p2repo.util;
 
 import java.util.IdentityHashMap;
@@ -8,18 +16,6 @@ import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
 
-/**
- * Copyright (c) 2006-2009, Cloudsmith Inc.
- * The code, documentation and other materials contained herein have been
- * licensed under the Eclipse Public License - v 1.0 by the copyright holder
- * listed above, as the Initial Contributor under such license. The text of
- * such license is available at www.eclipse.org.
- */
-
-/**
- * @author filip.hrbek@cloudsmith.com
- *
- */
 public class P2RepoUtil extends Plugin {
 	private static Plugin plugin;
 
